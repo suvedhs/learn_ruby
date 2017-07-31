e4df210ef3d6e22d598a1ceffceccf91e443baf1
Suvedh Srikanth <suvedhs@gmail.com>
2017-07-31 07:46:02 -0500

    finished 2

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,40 @@
 #write your code here
+def add first, second
+  first+second
+end
+
+def subtract first, second
+  first - second
+end
+
+def sum array_of_nums
+  total = 0
+  iterator = 0
+  while iterator < array_of_nums.size
+    total = total + array_of_nums[iterator]
+    iterator+=1
+  end
+  total
+end
+
+def multiply (*args)
+  total = 1
+  iterator = 0
+  while iterator < args.size
+    total *= args[iterator]
+    iterator+=1
+  end
+  total
+end
+
+def power first, second
+  first**second
+end
+
+def factorial num
+  if num == 0
+    1
+  else
+    num*factorial(num-1)
+  end
+end
