f37611d2a46a3dedcae6ea9330f759c6580c9ff1
Suvedh Srikanth <suvedhs@gmail.com>
2017-07-31 14:45:16 -0500

    project complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+  def initialize
+  end
+
+  def title= new_title
+    exclude_capitalization = ["the","a","an","and","in","of"]
+    title_words = new_title.split
+    iterator = 1
+    @title = title_words[0].capitalize
+    while iterator < title_words.size
+      if !exclude_capitalization.include? title_words[iterator]
+        title_words[iterator] = title_words[iterator].capitalize
+      end
+      @title += " " + title_words[iterator]
+      iterator += 1
+    end
+  end
+
+  def title
+    @title
+  end
+  #title = title.upcase
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,38 @@
 class Timer
   #write your code here
+  attr_accessor :seconds, :time_string
+
+  def initialize
+    @seconds = 0
+    @time_string = "00:00:00"
+  end
+
+  def seconds= num_seconds
+    @seconds = num_seconds
+    @time_string = ""
+    hours = num_seconds / 3600
+    if hours < 10
+      @time_string += "0"
+    end
+    @time_string += hours.to_s + ":"
+    num_seconds %= 3600
+    minutes = num_seconds / 60
+    if minutes < 10
+      @time_string += "0"
+    end
+    @time_string += minutes.to_s + ":"
+    num_seconds = num_seconds % 60
+    if num_seconds < 10
+      @time_string += "0"
+    end
+    @time_string += num_seconds.to_s
+
+  end
+  def seconds
+    @seconds
+  end
+  def time_string
+    @time_string
+  end
+
 end
