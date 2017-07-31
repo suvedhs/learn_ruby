323b78d35934e390d89978d0112fbef98ee161a0
Suvedh Srikanth <suvedhs@gmail.com>
2017-07-31 07:26:23 -0500

    finished first two

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
 #write your code here
+def ftoc fahrenheit
+  fahrenheit -= 32
+  celsius = fahrenheit*5.0/9
+  celsius
+end
+
+def ctof celsius
+  fahrenheit = celsius*9.0/5
+  fahrenheit+=32
+  fahrenheit
+end
